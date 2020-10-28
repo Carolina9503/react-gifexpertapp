850e373de200d04acefb5d2896de19658c7360a9
Carolina <daza.jeymmy@gmail.com>
2020-10-27 22:49:07 -0500

    docs agregado

---
+++ docs/precache-manifest.77ef9b9299da8f7846f84834c9666ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2d517f460ca7157642f2f59503990a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8359eddf61fcd068e041",
+    "url": "/static/css/main.a5a1d1af.chunk.css"
+  },
+  {
+    "revision": "a45c0ff7c9e28121c691",
+    "url": "/static/js/2.3e75c129.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.3e75c129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8359eddf61fcd068e041",
+    "url": "/static/js/main.e0e86332.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
