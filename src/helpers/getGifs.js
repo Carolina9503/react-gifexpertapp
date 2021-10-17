1b7875205e12e14ae082751f5fba8f11b3e7ab3c
Carolina9503 <69082421+Carolina9503@users.noreply.github.com>
2021-10-17 04:19:06 -0500

    dirección de gifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,5 @@
 export const getGifs = async(category) => {
-
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=JW87SvB06JuJl1Bl1FBtLnBRh5I421oa`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10 &api_key=QyH6A4AfUvmO99PySpTUJEH6cQPjAmxG`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
